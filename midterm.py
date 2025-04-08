120d9246c3193a04e91f86b024d6899123617d6d
Jake Cohen <jcohen0121@gmail.com>
2025-04-08 10:49:41 -0400

    added logic to find which stats

--- midterm.py
+++ midterm.py
@@ -7,11 +7,19 @@ row = data.columns[0]
 print(f"available teams ({row}):", data[row].tolist())
 print("available stats:", list(data.columns))
 
-user_team_input = input('which teams stadium would you like to see?: ')
-user_stat_input = input('which stats would you like to see?: ')
+row_name = input('which teams stadium would you like to see?: ')
+col_name = input('which stats would you like to see?: ')
 
-#if user_team_input in data['team_name']:
-    #if user_
+if user_team_input not in data['team_name']:
+    print("team not found")
+else:
+    row_name = data[data[row] == user_team_input]
+
+    if not row.empty:
+        value = row.iloc[0][user_stat_input]
+        print(f"The stat you are looking for for '{user_team_input}' is '{user_stat_input}' : {value}" )
+    else:
+        print("stat metric not found")
 
 
 
