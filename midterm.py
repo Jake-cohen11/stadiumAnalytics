672a2357b71438331b60ae09ca978c75e5760720
Jake Cohen <jcohen0121@gmail.com>
2025-04-09 17:29:09 -0400

    added a metric finder function as well as getting rid of previous code

--- midterm.py
+++ midterm.py
@@ -6,23 +6,14 @@ def read_csv(file):
     data.set_index('team_name', inplace=True)
     return data
 
+def get_stat(data, acronym, metric):
+     try:
+         value = data.loc[acronym.upper(), metric]
+         return value
+     except KeyError:
+         return "Team or metric not found, please refer to the list."
 
-row = data.columns[0]
-print(f"available teams ({row}):", data[row].tolist())
-print("available stats:", list(data.columns))
 
-row_name = input('which teams stadium would you like to see?: ')
-col_name = input('which stats would you like to see?: ')
 
-if row_name not in data:
-    print("team not found")
-else:
-    row_name = data[data[row] == row_name]
-
-    if not row.empty:
-        value = row.iloc[0][col_name]
-        print(f"The stat you are looking for for '{row_name}' is '{col_name}' : {value}" )
-    else:
-        print("stat metric not found")
 
 sub_data = data[['team_name', 'ballpark', 'left_field', 'center_field', 'right_field']]
