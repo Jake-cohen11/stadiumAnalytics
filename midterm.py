c536afecf9142a7610c9d2b1d59f26c5f53ca496
Jake Cohen <jcohen0121@gmail.com>
2025-04-09 18:06:15 -0400

    added lower to metric search

--- midterm.py
+++ midterm.py
@@ -8,7 +8,7 @@ def read_csv(file):
 
 def get_stat(data, acronym, metric):
      try:
-         value = data.loc[acronym.upper(), metric]
+         value = data.loc[acronym.upper(), metric.lower()]
          return value
      except KeyError:
          return "Team or metric not found, please refer to the list."
