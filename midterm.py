ee8469b5266f2de09999f475bb82460d1a469cea
Jake Cohen <jcohen0121@gmail.com>
2025-04-09 17:26:55 -0400

    created a function for the file

--- midterm.py
+++ midterm.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv('ballparks.csv')
-data.shape
+
+def read_csv(file):
+    data = pd.read_csv(file)
+    data.set_index('team_name', inplace=True)
+    return data
+
 
 row = data.columns[0]
 print(f"available teams ({row}):", data[row].tolist())
@@ -10,19 +14,15 @@ print("available stats:", list(data.columns))
 row_name = input('which teams stadium would you like to see?: ')
 col_name = input('which stats would you like to see?: ')
 
-if user_team_input not in data['team_name']:
+if row_name not in data:
     print("team not found")
 else:
-    row_name = data[data[row] == user_team_input]
+    row_name = data[data[row] == row_name]
 
     if not row.empty:
-        value = row.iloc[0][user_stat_input]
-        print(f"The stat you are looking for for '{user_team_input}' is '{user_stat_input}' : {value}" )
+        value = row.iloc[0][col_name]
+        print(f"The stat you are looking for for '{row_name}' is '{col_name}' : {value}" )
     else:
         print("stat metric not found")
-
-
-
-
 
 sub_data = data[['team_name', 'ballpark', 'left_field', 'center_field', 'right_field']]
